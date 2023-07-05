4cf57bcbb98daded37b0e262b7a57d3f594ecb7c
rhaversen <rhaversen@gmail.com>
2023-07-06 01:51:06 +0200

    require .env

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { 
     HashingError
   } = require('../utils/errors');
--- utils/logger.js
+++ utils/logger.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const winston = require('winston');
 const path = require('path');
 
