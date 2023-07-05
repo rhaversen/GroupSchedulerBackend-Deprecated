546bd1eac9a619cf8b91f2dee544796de5276cae
rhaversen <rhaversen@gmail.com>
2023-07-05 21:03:25 +0200

    add file extension

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.SERVER_PORT;
 const mongoose = require('mongoose');
 
 const path = require('path');
-const logger = require('./utils/logger');
+const logger = require('./utils/logger.js');
 
 const express = require('express');
 const app = express();
