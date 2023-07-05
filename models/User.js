4120074f1397e2b543cde376c1e54e8b4d42de02
rhaversen <rhaversen@gmail.com>
2023-07-05 04:26:30 +0200

    Fixed logging, added comments to schema fields

--- models/User.js
+++ models/User.js
@@ -11,26 +11,26 @@ const saltRounds = process.env.BCRYPT_SALT_ROUNDS
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: { type: String, required : true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { type: String }, // This is how other users will recognize you. It should reflect your name or nickname. Dont worry, only users in the same events as you can see your name.
+    email: { type: String, required: true, unique: true }, // This is how you will log in, no users will be able to see this
+    password: { type: String, required: true }, 
     events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
-    availabilities: [{ type: Schema.Types.ObjectId, ref: 'Availability' }]
+    availabilities: [{ type: Schema.Types.ObjectId, ref: 'Availability' }],
 });
 
 // Password hashing middleware
 userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        next();
-        return;
+    if (this.isModified('password')) {
+        try {
+            const salt = await bcrypt.genSalt(saltRounds); //gensalt and hash is already async
+            this.password = await bcrypt.hash(this.password, salt);
+            return next();
+        } catch (err) {
+            return next(new HashingError('Error generating a password hash', err));
+        }
     }
-    try {
-        const salt = await bcrypt.genSalt(saltRounds); //gensalt and hash is already async
-        this.password = await bcrypt.hash(this.password, salt);
-        return next();
-    } catch (err) {
-        return next(new HashingError('Error generating a password hash', err));
-    }
+
+    logger.info('User saved')
 });
 
 // Method for comparing passwords
