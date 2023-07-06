93ffaeecc77033cf06fee1f7abb6d986105ea6a2
rhaversen <rhaversen@gmail.com>
2023-07-06 03:33:10 +0200

    Migrate to ES

--- middleware/globalErrorHandler.js
+++ middleware/globalErrorHandler.js
@@ -1,26 +1,25 @@
-const { 
-    ValidationError,
-    DatabaseError,
-    ServerError
-  } = require('../utils/errors.mjs').default;
+import errors from '../utils/errors.mjs';
+const {
+    ValidationError, DatabaseError, ServerError
+} = errors;
 
-const logger = require('../utils/logger.js');
+import { warn, error as _error } from '../utils/logger.js';
 
-module.exports = (function(err, req, res, next) {
+export default (function(err, req, res, next) {
     if (err instanceof ValidationError) {
         // These are client-safe errors that can be directly sent to the client.
         res.status(400).json({ error: err.message });
-        logger.warn(err.message)
+        warn(err.message)
     } else if (err instanceof EventError) {
         res.status(400).json({ error: err.message });
-        logger.error(err.message);
+        _error(err.message);
     } else if (err instanceof DatabaseError || err instanceof ServerError) {
         // For server-side errors, send a generic error message
         res.status(500).json({ error: 'An error occurred, please try again later' });
-        logger.error(err.message);
+        _error(err.message);
     } else {
         // If it's not one of the known errors, it could be anything - consider it a 500 error
         res.status(500).json({ error: 'An error occurred, please try again later' });
-        logger.error(err.message);
+        _error(err.message);
     }
 });
