8a2feb8844485603da976dc2d1559e4c17201423
rhaversen <rhaversen@gmail.com>
2023-07-07 18:50:19 +0200

    async/await

--- test/server.test.js
+++ test/server.test.js
@@ -7,46 +7,36 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('GET /', () => {
-  it(' should return the index page', function() {
+  it(' should return the index page', async function() {
     this.timeout(10000); // Set the timeout to 10 seconds.
 
-    return import('../server.mjs').then(async (server) => {
-      const res = await chai.request(server.default).get('/');
-      expect(res).to.have.status(200);
-    });
+    const server = await import('../server.mjs');
+    const res = await chai.request(server.default).get('/');
+    expect(res).to.have.status(200);
   });
 });
 
-
 describe('POST /api/v1/users', () => {
-    it('should create a new user and return a jwt token', function(done) {
-      import('../server.mjs').then((server) => {
+  it('should create a new user and return a jwt token', async function() {
+    this.timeout(10000); // Set the timeout to 10 seconds.
 
-        this.timeout(10000); // Set the timeout to 5 seconds.
-
-        const newUser = {name: 'Test User', email: 'testuser@gmail.com', password: 'testpassword' };
+    const server = await import('../server.mjs');
     
-        chai
-          .request(server)
-          .post('/api/v1/users')
-          .send(newUser)
-          .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(201);
-            expect(res.body).to.be.a('object');
-            expect(res.body).to.have.property('token');
-            
-            const token = res.body.token;
-            
-            // Here, you can further test the JWT token if needed, for example:
-            // - Verify the token's signature
-            // - Decode the token and check its payload
-            
-            // For simplicity, let's just check if the token exists
-            expect(token).to.not.be.empty;
+    const newUser = {name: 'Test User', email: 'testuser@gmail.com', password: 'testpassword' };
 
-            done();
-          });
-      });
-    });
+    const res = await chai.request(server.default).post('/api/v1/users').send(newUser);
+      
+      expect(res).to.have.status(201);
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.have.property('token');
+      
+      const token = res.body.token;
+      
+      // Here, you can further test the JWT token if needed, for example:
+      // - Verify the token's signature
+      // - Decode the token and check its payload
+      
+      // For simplicity, let's just check if the token exists
+      expect(token).to.not.be.empty;
   });
+});
