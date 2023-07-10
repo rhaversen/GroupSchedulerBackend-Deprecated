d869d8dd3a206571ae07947c201adccb0c15ebce
rhaversen <rhaversen@gmail.com>
2023-07-10 06:30:04 +0200

    use memory database

--- database.mjs
+++ database.mjs
@@ -1,47 +1,46 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import 'dotenv/config';
+import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+
+// Own modules
+import logger from './utils/logger.mjs';
+
+const mongooseOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
 
 let mongoServer;
 
 const connectToDatabase = async () => {
-  const env = process.env.NODE_ENV;
-
-  // Use in-memory database for testing
-  if (env === 'test') {
-    mongoServer = new MongoMemoryServer();
-    const mongoUri = await mongoServer.getUri();
-
-    try {
-      await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
-      logger.info('Connected to in-memory MongoDB...');
-    } catch (error) {
-      logger.error(`Error connecting to in-memory MongoDB: ${error.message}`);
+    if (process.env.NODE_ENV === 'test') {
+        mongoServer = await MongoMemoryServer.create();
+        const mongoUri = mongoServer.getUri();
+        await mongoose.connect(mongoUri, mongooseOpts);
+        logger.info('Connected to in-memory MongoDB');
+    } else {
+        logger.info(`Attempting to connect to to MongoDB`);
+        const maxRetryAttempts = process.env.DB_CONNECTION_RETRY_MAX_ATTEMPTS || 5;
+        const retryInterval = process.env.DB_CONNECTION_RETRY_INTERVAL || 1000;
+        const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
+    
+        let currentRetryAttempt = 0;
+    
+        while (currentRetryAttempt < maxRetryAttempts) {
+        try {
+            await mongoose.connect(mongoUri, mongooseOpts);
+            logger.info('MongoDB Connected...');
+            return; // Exit the function if the connection is successful
+        } catch (error) {
+            logger.error(`Error connecting to MongoDB: ${error.message}`);
+            currentRetryAttempt++;
+            await new Promise((resolve) => setTimeout(resolve, retryInterval));
+        }}
+        logger.error(`Failed to connect to MongoDB after ${maxRetryAttempts} attempts.`);
     }
-  }
-
-  // Use real database for development and production
-  else {
-    logger.info(`Attempting to connect to to MongoDB`);
-    const maxRetryAttempts = process.env.DB_CONNECTION_RETRY_MAX_ATTEMPTS || 5;
-    const retryInterval = process.env.DB_CONNECTION_RETRY_INTERVAL || 1000;
-  
-    let currentRetryAttempt = 0;
-  
-    while (currentRetryAttempt < maxRetryAttempts) {
-      try {
-        await mongoose.connect(
-          `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`, 
-          { useNewUrlParser: true, useUnifiedTopology: true }
-        );
-        logger.info('MongoDB Connected...');
-        return; // Exit the function if the connection is successful
-      } catch (error) {
-        logger.error(`Error connecting to MongoDB: ${error.message}`);
-        currentRetryAttempt++;
-        await new Promise((resolve) => setTimeout(resolve, retryInterval));
-      }
-    }
-    logger.error(`Failed to connect to MongoDB after ${maxRetryAttempts} attempts.`);
-  }
 };
 
 const disconnectFromDatabase = async () => {
