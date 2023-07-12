7ed538327dfb9449a72d8fc967c4ca7096834f51
rhaversen <rhaversen@gmail.com>
2023-07-12 17:08:47 +0200

    Include description in availability

--- models/Availability.mjs
+++ models/Availability.mjs
@@ -2,6 +2,7 @@ import mongoose, { model } from 'mongoose';
 const { Schema } = mongoose;
 
 const availabilitySchema = new Schema({
+  description: { type: String },
   date: { type: Date, required: true },
   status: { type: String, enum: ['Free', 'Busy', 'Maybe'], required: true },
   preference: { type: Number, min: 1, max: 3, default: 2 }, // 1-3 scale for preference. 1: Free, but prefer not to. 2: Normal preference. 3: Strongly prefer. Strongly preferring every availability will have the same effect as normal preference, so be selective.
