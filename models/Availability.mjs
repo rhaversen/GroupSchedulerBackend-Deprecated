77184fa97b7b2d3b073481425ec635e19e2b361b
rhaversen <rhaversen@gmail.com>
2023-07-16 14:38:59 +0100

    remove user from Availability schema

--- models/Availability.mjs
+++ models/Availability.mjs
@@ -2,11 +2,10 @@ import mongoose, { model } from 'mongoose';
 const { Schema } = mongoose;
 
 const availabilitySchema = new Schema({
-  description: { type: String },
+  description: { type: String }, // This is public for anyone in the same event as you if the date is within the events date-range
   date: { type: Date, required: true },
   status: { type: String, enum: ['Free', 'Busy', 'Maybe'], required: true },
   preference: { type: Number, min: 1, max: 3, default: 2 }, // 1-3 scale for preference. 1: Free, but prefer not to. 2: Normal preference. 3: Strongly prefer. Strongly preferring every availability will have the same effect as normal preference, so be selective.
-  user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 export default model('Availability', availabilitySchema);
