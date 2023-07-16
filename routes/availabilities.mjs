dcde08ea45a40095195945e957dea7dce25e3744
rhaversen <rhaversen@gmail.com>
2023-07-16 14:40:07 +0100

    availabilities route uses controller, fix imports

--- routes/availabilities.mjs
+++ routes/availabilities.mjs
@@ -1,38 +1,31 @@
 // Node.js built-in modules
 
 // Third-party libraries
-import { Router } from 'express';
+import Router from 'express';
 import passport from 'passport';
 
 // Own modules
-import errors from '../utils/errors.mjs';
-import logger from '../utils/logger.mjs';
-import Availability from '../models/Availability.mjs';
-import User from '../models/User.mjs';
-import {
-    getEvent,
-    checkUserInEvent,
-    checkUserIsAdmin
-} from '../middleware/availabilityFunctions.mjs';
 import {
     sanitizeInput,
-  } from '../middleware/sanitizer.mjs';
+} from '../middleware/sanitizer.mjs';
+
+// Controller functions
+import {
+    newOrUpdateAvailability
+} from '../middleware/availabilityController.mjs';
 
 // Destructuring and global variables
-const {
-    MissingFieldsError,
-} = errors;
 const router = Router();
 
 /**
  * @route PUT api/v1/users/availabilities
- * @desc Update event with a random event code
+ * @desc Create a new availability or update existing if the date is the same.
  * @access AUTHENTICATED
 */
 router.put('/',
     passport.authenticate('jwt', { session: false }),
     sanitizeInput,
-    
+    newOrUpdateAvailability
 );
 
 export default router;
