1ffe4ca010e10e3987ef97cefb370d1f29b7df31
rhaversen <rhaversen@gmail.com>
2023-08-09 04:30:08 +0200

    simpler custom error class

--- utils/errors.mjs
+++ utils/errors.mjs
@@ -1,138 +1,55 @@
-//Validation errors
-class ValidationError extends Error {
-  constructor(message) {
+class CustomError extends Error {
+  constructor(message, statusCode = 400) {
     super(message);
     this.name = this.constructor.name;
-    this.statusCode = 400;
+    this.statusCode = statusCode;
   }
 }
+
+// Validation errors
+class ValidationError extends CustomError {}
 
 class UserNotFoundError extends ValidationError {
   constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 404;
+    super(message, 404);
   }
 }
 
-class InvalidEmailError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-class InvalidPasswordError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-class EmailAlreadyExistsError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-class PasswordIncorrectError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
+class InvalidEmailError extends ValidationError {}
+class InvalidPasswordError extends ValidationError {}
+class EmailAlreadyExistsError extends ValidationError {}
+class PasswordIncorrectError extends ValidationError {}
+class MissingFieldsError extends ValidationError {}
+class InvalidConfirmationCodeError extends ValidationError {}
+class AlreadyConfirmedError extends ValidationError {}
 
 class UserNotInEventError extends ValidationError {
   constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 403;
+    super(message, 403);
   }
 }
 
 class UserNotAdminError extends ValidationError {
   constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 403;
+    super(message, 403);
   }
 }
 
-class InvalidEventIdOrCode extends ValidationError {
+class InvalidEventIdOrCode extends ValidationError {}
+
+// Event errors
+class EventError extends CustomError {}
+class EventCodeError extends EventError {}
+class EventNotFoundError extends EventError {}
+
+// Server errors
+class ServerError extends CustomError {
   constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
+    super(message, 500);
   }
 }
 
-class MissingFieldsError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-class InvalidConfirmationCodeError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-class AlreadyConfirmedError extends ValidationError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-//Event errors
-class EventError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 400;
-  }
-}
-
-class EventCodeError extends EventError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-  }
-}
-
-class EventNotFoundError extends EventError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-  }
-}
-
-//Server errors
-class ServerError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = 500;
-  }
-}
-
-class HashingError extends ServerError {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-  }
-}
+class HashingError extends ServerError {}
 
 export default {
   ValidationError,
