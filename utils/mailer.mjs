7ab7fd8f9d7e1b2b2ca2a0eadbfe1dfe8f1109fe
rhaversen <rhaversen@gmail.com>
2023-08-15 04:15:49 +0200

    Send html email

--- utils/mailer.mjs
+++ utils/mailer.mjs
@@ -1,32 +1,39 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Configure transporter
 const transporter = nodemailer.createTransport({
-  host: 'smtp.simply.com',
+  host: process.env.EMAIL_HOST,
   port: 465,    // Common port for SSL
   secure: true, // true for 465, false for other ports
   auth: {
-    user: 'rasmus@haversen.dk',
-    pass: 'Rasmus123',
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
 // Generic function to send email
-export const sendEmail = (to, subject, text) => {
+export const sendEmail = (to, subject, text, html = '') => {
   const mailOptions = {
     from: 'rasmus@haversen.dk',
     to,
     subject,
     text,
+    html
   };
 
   transporter.sendMail(mailOptions);
 };
 
+
 // Function to send confirmation email
 export const sendConfirmationEmail = (email, confirmationLink) => {
   const subject = 'Please confirm your email address';
-  const text = `Please confirm your email by clicking on the following link: ${confirmationLink}`;
+  const text = `Please confirm your email by pasting this link into your browser: ${confirmationLink} \n (Your email inbox does not support HTML)`;
+  const html = `<a href="${confirmationLink}">${confirmationLink}</a> <br> <p>Please confirm your email by clicking the link above.</p>`;
 
-  sendEmail(email, subject, text);
+  sendEmail(email, subject, text, html);
 };
+
