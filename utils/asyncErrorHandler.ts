6c8ac2132e795fac518d6ea104bd7b82942affe3
rhaversen <rhaversen@gmail.com>
2023-08-23 05:34:05 +0200

    Beter promise handling Fix return type, RequestHandler

--- utils/asyncErrorHandler.ts
+++ utils/asyncErrorHandler.ts
@@ -1,11 +1,16 @@
 // Third-party libraries
-import { type Request, type Response, type NextFunction } from 'express'
+import { RequestHandler, type Request, type Response, type NextFunction } from 'express'
 
 type AsyncMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
-const asyncErrorHandler = (fn: AsyncMiddleware) => (req: Request, res: Response, next: NextFunction) =>
-  Promise
-    .resolve(fn(req, res, next))
-    .catch(next);
+const asyncErrorHandler = (fn: AsyncMiddleware): RequestHandler => {
+    return (req: Request, res: Response, next: NextFunction): void => {
+        try {
+            Promise.resolve(fn(req, res, next)).catch(next);
+        } catch (err) {
+            next(err);
+        }
+    };
+};  
 
 export default asyncErrorHandler
