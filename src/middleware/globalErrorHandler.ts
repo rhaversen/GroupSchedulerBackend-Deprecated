51f4acc598094edae8da97db877ca0ad6789293e
rhaversen <rhaversen@gmail.com>
2023-10-19 21:57:19 +0200

    Combine similar cases

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -10,19 +10,16 @@ import {
 import logger from '../utils/logger.js'
 
 export default (function (err: any, req: Request, res: Response, next: NextFunction): void {
-    if (err instanceof ValidationError) {
-    // These are client-safe errors that can be directly sent to the client.
-        logger.silly(err.message)
-        res.status(400).json({ error: err.message })
-    } else if (err instanceof EventError) {
+    if (err instanceof ValidationError || err instanceof EventError) {
+        // These are client-safe errors that can be directly sent to the client.
         logger.silly(err.message)
         res.status(400).json({ error: err.message })
     } else if (err instanceof ServerError) {
-    // For server-side errors, send a generic error message
+        // For server-side errors, send a generic error message
         logger.error(err.message)
         res.status(500).json({ error: 'A server- or database-error occurred, please try again later' })
     } else {
-    // If it's not one of the known errors, it could be anything - consider it a 500 error
+        // If it's not one of the known errors, it could be anything - consider it a 500 error
         logger.error(err.toString())
         res.status(500).json({ error: 'An error occurred, please try again later' })
     }
