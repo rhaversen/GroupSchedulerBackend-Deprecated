9e1ef31b46212d642fa99753f140cea78605333a
rhaversen <rhaversen@gmail.com>
2023-11-16 00:36:21 +0100

    try whole for loop at once

--- src/utils/vault.ts
+++ src/utils/vault.ts
@@ -4,22 +4,22 @@ import logger from './logger.js'
 let vault: client
 
 export async function connectToVault () {
-try {
+    try {
         logger.silly('importing node-vault')
-    const NodeVault = await import('node-vault')
-    vault = NodeVault.default({
-        endpoint: process.env.VAULT_ADDR, // Injected with initial .env
-        token: process.env.VAULT_TOKEN // Injected with initial .env
-    })
+        const NodeVault = await import('node-vault')
+        vault = NodeVault.default({
+            endpoint: process.env.VAULT_ADDR, // Injected with initial .env
+            token: process.env.VAULT_TOKEN // Injected with initial .env
+        })
         logger.silly('Checking vault health')
         logger.info('Vault health: ' + await vault.health())
-    logger.info("Connected to node-vault!")
-} catch (e) {
-    if (process.env.NODE_ENV === 'production') {
-        logger.error('node-vault is required in production')
-        process.exit(1)
-    }
-    logger.warn('node-vault is not installed. Falling back to .env')
+        logger.info("Connected to node-vault!")
+    } catch (e) {
+        if (process.env.NODE_ENV === 'production') {
+            logger.error('node-vault is required in production')
+            process.exit(1)
+        }
+        logger.warn('node-vault is not installed. Falling back to .env')
     }
 }
 
@@ -40,18 +40,18 @@ export async function loadSecrets () {
     logger.silly('determining node env')
     if (process.env.NODE_ENV === 'production') {
         logger.info('Loading secrets')
-        for (const key of keys) {
-            try {
+        try {
+            for (const key of keys) {
                 logger.silly('Loading secret for key ' + key)
                 const secretResponse = await vault.read(`secret/data/backend/${key}`)
                 const secret = secretResponse.data.data;
                 process.env[key] = secret.value;
-            } catch (err) {
-                logger.error(`Failed to load secrets: ${err}`)
-                logger.error(`Shutting down`)
-                process.exit(1)
-            }
-        }   
+            }   
+        } catch (err) {
+            logger.error(`Failed to load secrets: ${err}`)
+            logger.error(`Shutting down`)
+            process.exit(1)
+        }
     } else {
         logger.info('Using development .env')
         // .env values are already loaded by dotenv
