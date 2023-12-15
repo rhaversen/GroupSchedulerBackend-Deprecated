26dcb409b218e9f541147b11b9703f8c38e41229
rhaversen <rhaversen@gmail.com>
2023-12-16 00:25:59 +0100

    Status not required, will default to busy

--- src/models/Availability.ts
+++ src/models/Availability.ts
@@ -11,7 +11,7 @@ export interface IAvailability extends Document {
 // TODO: If an availability is not set, meaning you are undecided on that date, it will be assumed to be free. Before the date for the event is locked, the user will be warned that not all users are decided, if thats the case.
 const availabilitySchema = new Schema<IAvailability>({
     date: { type: Date, required: true },
-    status: { type: String, enum: ['Free', 'Busy', 'Maybe'], required: true },
+    status: { type: String, enum: ['Free', 'Busy', 'Maybe'], default: 'Busy' },
     preference: { type: Number, min: 1, max: 3, default: 2 } // 1-3 scale for preference. 1: Free, but prefer not to. 2: Default/Normal preference. 3: Strongly prefer. Strongly preferring every availability will have the same effect as normal preference, so be selective.
 })
 
